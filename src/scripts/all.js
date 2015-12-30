d876b6479554ca244569db87d8e79442e5793418
Zeno Rocha <hi@zenorocha.com>
2015-12-30 12:18:50 -0800

    Separates toggler logics into a separated file

--- src/scripts/all.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-	setTimeout(function() {
-		document.body.classList.remove('fade-out');
-		document.body.classList.add('fade-in');
-	}, 0);
-});
---
+++ src/scripts/menu.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+	window.toggler = new metal.Toggler({
+		content: '.sidebar-list-1',
+		header: '.sidebar-header'
+	});
+});
