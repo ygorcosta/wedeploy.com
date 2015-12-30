3555bb9de0f2c12dbc33e4f68154ac6ad8b40bf6
Zeno Rocha <hi@zenorocha.com>
2015-12-30 10:05:44 -0800

    Build

--- build/scripts/docs.js
+++ build/scripts/docs.js
@@ -2,6 +2,35 @@ document.addEventListener('DOMContentLoaded', function() {
 	if (window.hljs) {
 		window.hljs.initHighlightingOnLoad();
 	}
+
+	var alert = new metal.Alert({
+		visible: false,
+		animClasses: {
+			hide: 'hidden'
+		}
+	}).render();
+
+	var form = document.querySelector('.newsletter-form');
+
+	form.addEventListener('submit', function(e) {
+		e.preventDefault();
+
+		Launchpad.url('http://liferay.io/newsletter/subscribers')
+			.post({
+				email: e.target.email.value,
+				timestamp: new Date().toISOString()
+			})
+			.then(function(result) {
+				alert.body = 'Thanks for subscribing!';
+				alert.elementClasses = 'alert-success';
+				alert.visible = true;
+			})
+			.catch(function(error) {
+				alert.body = 'Something wrong happened! Please come back later.';
+				alert.elementClasses = 'alert-danger';
+				alert.visible = true;
+			});
+	});
 });
 
 window.toggler = new metal.Toggler({
