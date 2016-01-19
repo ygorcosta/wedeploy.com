dc519fbf9739e1329067d518d828971621ea514e
Zeno Rocha <hi@zenorocha.com>
2016-01-19 11:22:04 -0800

    Adds autocomplete logics

--- src/scripts/guide.js
+++ src/scripts/guide.js
@@ -1,3 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
 	window.hljs.initHighlightingOnLoad();
+
+	var input = document.querySelector('.docs-search-form .form-control');
+
+	new metal.Autocomplete({
+		inputElement: input,
+		data: function(query) {
+			var docs = Launchpad.url('http://liferay.io/docs/search').path(input.dataset.lang);
+
+			if (query) {
+				docs.search('*', 'prefix', query).limit(3).highlight('content');
+			}
+
+			return docs.get().then(function(response) {
+				return response.body().documents;
+			});
+		},
+		format: function(item) {
+			console.log(item);
+			return {
+				textPrimary: metal.SoyRenderer.sanitizeHtml('<a class="autocomplete-link" href=".' + item.path + '">' +
+					'<p class="autocomplete-title">' + item.title + '</p>' +
+					'<p class="autocomplete-text">' + item.content.substr(0, 100) + '...</p></a>')
+			};
+		},
+		select: function() {}
+	}).render();
 });
