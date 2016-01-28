1f503012c1e88e470025a6469fbf73540d34f2c8
Zeno Rocha <hi@zenorocha.com>
2016-01-28 10:36:00 -0800

    Adds autocomplete into docs home search

--- src/scripts/docs.js
+++ src/scripts/docs.js
@@ -40,15 +40,49 @@ form.addEventListener('submit', function(e) {
    Sidebar Search
    ========================================================================== */
 
-if (autocomplete) {
-	autocomplete.dispose();
-	autocomplete = null;
+if (sidebarAutocomplete) {
+	sidebarAutocomplete.dispose();
+	sidebarAutocomplete = null;
 }
 
-var input = document.querySelector('.sidebar-search .form-control');
+var sidebarInput = document.querySelector('.sidebar-search .form-control');
 
-var autocomplete = new metal.Autocomplete({
-	inputElement: input,
+var sidebarAutocomplete = new metal.Autocomplete({
+	inputElement: sidebarInput,
+	data: function(query) {
+		var docs = Launchpad.url('http://liferay.io/docs/search/all');
+
+		if (query) {
+			docs.search('*', 'prefix', query).limit(3).highlight('content');
+		}
+
+		return docs.get().then(function(response) {
+			return response.body().documents;
+		});
+	},
+	format: function(item) {
+		return {
+			textPrimary: metal.SoyRenderer.sanitizeHtml('<a class="autocomplete-link" href=".' + item.path + '">' +
+				'<p class="autocomplete-title">' + item.title + '</p>' +
+				'<p class="autocomplete-text">' + item.content.substr(0, 100) + '...</p></a>')
+		};
+	},
+	select: function() {}
+}).render();
+
+/* ==========================================================================
+   Sidebar Search
+   ========================================================================== */
+
+if (mainAutocomplete) {
+	mainAutocomplete.dispose();
+	mainAutocomplete = null;
+}
+
+var mainInput = document.querySelector('.docs-home-top .form-control');
+
+var mainAutocomplete = new metal.Autocomplete({
+	inputElement: mainInput,
 	data: function(query) {
 		var docs = Launchpad.url('http://liferay.io/docs/search/all');
 
