03bf0cd80b6f2c26a7ea256e2774997139ee3c40
Zeno Rocha <hi@zenorocha.com>
2016-02-03 15:44:55 -0800

    Adds newsletter on search page

--- build/scripts/docs.js
+++ build/scripts/docs.js
@@ -1,42 +1,4 @@
 /* ==========================================================================
-   Newsletter
-   ========================================================================== */
-
-if (docsAlert) {
-	docsAlert.dispose();
-	docsAlert = null;
-}
-
-var docsAlert = new metal.Alert({
-	visible: false,
-	animClasses: {
-		hide: 'hidden'
-	}
-}).render();
-
-var form = document.querySelector('.docs-home-newsletter-form');
-
-form.addEventListener('submit', function(e) {
-	e.preventDefault();
-
-	Launchpad.url('http://liferay.io/newsletter/subscribers')
-		.post({
-			email: e.target.email.value,
-			timestamp: new Date().toISOString()
-		})
-		.then(function(result) {
-			docsAlert.body = 'Thanks for subscribing!';
-			docsAlert.elementClasses = 'alert-success';
-			docsAlert.visible = true;
-		})
-		.catch(function(error) {
-			docsAlert.body = 'Something wrong happened! Please come back later.';
-			docsAlert.elementClasses = 'alert-danger';
-			docsAlert.visible = true;
-		});
-});
-
-/* ==========================================================================
    Sidebar Search
    ========================================================================== */
 
---
+++ build/scripts/newsletter.js
@@ -0,0 +1,37 @@
+/* ==========================================================================
+   Newsletter
+   ========================================================================== */
+
+if (newsletterAlert) {
+	newsletterAlert.dispose();
+	newsletterAlert = null;
+}
+
+var newsletterAlert = new metal.Alert({
+	visible: false,
+	animClasses: {
+		hide: 'hidden'
+	}
+}).render();
+
+var newsletterForm = document.querySelector('.newsletter-form');
+
+newsletterForm.addEventListener('submit', function(e) {
+	e.preventDefault();
+
+	Launchpad.url('http://liferay.io/newsletter/subscribers')
+		.post({
+			email: e.target.email.value,
+			timestamp: new Date().toISOString()
+		})
+		.then(function(result) {
+			newsletterAlert.body = 'Thanks for subscribing!';
+			newsletterAlert.elementClasses = 'alert-success';
+			newsletterAlert.visible = true;
+		})
+		.catch(function(error) {
+			newsletterAlert.body = 'Something wrong happened! Please come back later.';
+			newsletterAlert.elementClasses = 'alert-danger';
+			newsletterAlert.visible = true;
+		});
+});
