8c62d8503b0f1a071ab5a5a298440420f85dd287
ygo.costa <ygor.costa@liferay.com>
2016-05-31 19:12:17 -0300

    Changes some colors and Improves form validation

--- src/scripts/home/main.js
+++ src/scripts/home/main.js
@@ -59,28 +59,48 @@ $(document).ready(function() {
             emailInput.addClass('error');
             emailFeedback.addClass('active');
             emailFeedback.text('*Please enter an email address.');
-        }
-        else if (!isValidEmailAddress(emailInput.val())) {
-            emailInput.addClass('error');
-            emailFeedback.addClass('active');
-            emailFeedback.text('*Please enter a full email address, including the ‘@’..');
-        }
-        else {
-            emailInput.val('');
-            emailInput.removeClass('error');
-            emailFeedback.removeClass('active');
-            emailFeedback.text('');
+        } else {
+            if (!emailHasAt(emailInput.val())) {
+                emailInput.addClass('error');
+                emailFeedback.addClass('active');
+                emailFeedback.text('*Please enter a full email address, including the ‘@’..');
+            } else {
+                if (!emailHasDomain(emailInput.val())) {
+                    emailInput.addClass('error');
+                    emailFeedback.addClass('active');
 
-            isValid = true;
+                    var _email = emailInput.val();
+                    var _split = _email.split('@');
+                    var _domain = _split[1];
+
+                    if(_domain == "") {
+                        emailFeedback.text('*Do not forget the part after the @.');
+                    } else {
+                        emailFeedback.text('*This email address contain an invalid domain name \'' + _domain + '\'');
+                    }
+                } else {
+                    emailInput.val('');
+                    emailInput.removeClass('error');
+                    emailFeedback.removeClass('active');
+                    emailFeedback.text('');
+
+                    isValid = true;
+                }
+            }
         }
 
         return isValid;
     }
 
-    function isValidEmailAddress(emailAddress) {
-        var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    function emailHasAt(emailAddress) {
+        var pattern = new RegExp(/^[+a-zA-Z0-9._-]+@/i);
         return pattern.test(emailAddress);
-    };
+    }
+
+    function emailHasDomain(emailAddress) {
+        var pattern = new RegExp(/^[+a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i);
+        return pattern.test(emailAddress);
+    }
 
     function closeModal() {
         $('.modal').removeClass('active');
