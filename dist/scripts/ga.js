0741ddaf46476aeaa7029a34109e538b57a86d1f
Ivan Santos <pragmaticivan@gmail.com>
2016-10-19 10:18:02 -0700

    Remove unused dist

--- dist/scripts/ga.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var app = senna.dataAttributeHandler.getApp();
-
-  app.on('endNavigate', function(event) {
-    ga('set', 'page', event.path);
-    ga('send', 'pageview');
-  });
-});
