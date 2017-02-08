6fe9c6776e30f48575f6efebaedd1191d876cbb0
ygor.costa <ygor.costa@liferay.com>
2017-02-08 14:48:47 -0800

    Improves reading-progress

--- ui/src/scripts/reading-progress.js
+++ ui/src/scripts/reading-progress.js
@@ -1,14 +1,13 @@
 /* ==========================================================================
 	 Reading Progress Affix
 	 ========================================================================== */
-
 if (affix) {
 	affix.dispose();
 	affix = null;
 }
 
 var topicsElement = document.querySelector('.guide-topics');
-	if(topicsElement) {
+if(topicsElement) {
 	var docsNavOffsetTop = topicsElement ? 450 : 330;
 	var docsNavOffsetBottom = 60;
 
@@ -18,3 +17,35 @@ var topicsElement = document.querySelector('.guide-topics');
 		offsetBottom: docsNavOffsetBottom
 	});
 }
+
+/* ==========================================================================
+	 Reading Progress Resize Width
+	 ========================================================================== */
+
+document.addEventListener("DOMContentLoaded", function(event) {
+	resizeReadingProgress();
+
+	// TODO : check why senna.dataAttributeHandler.getApp() is starting null
+	var timeoutId = setTimeout(() => {
+		var app = senna.dataAttributeHandler.getApp();
+		if(app) {
+			clearTimeout(timeoutId);
+			app.on('endNavigate', endNavigate);
+		}
+	}, 500);
+});
+
+function endNavigate(e) {
+	resizeReadingProgress();
+}
+
+function resizeReadingProgress() {
+	var readingProgressContent = document.querySelector('.docs-nav-container');
+	var docsNav = readingProgressContent.childNodes[0];
+	docsNav.style.width = readingProgressContent.offsetWidth + "px";
+
+	window.addEventListener('resize', function() {
+		docsNav.style.width = readingProgressContent.offsetWidth + "px";
+	});
+}
+
