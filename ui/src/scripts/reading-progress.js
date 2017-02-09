181e8536082246c6f8bd0ccfa88df5f58ce902d4
ygor.costa <ygor.costa@liferay.com>
2017-02-09 15:43:37 -0800

    Fixes Toast blink

--- ui/src/scripts/reading-progress.js
+++ ui/src/scripts/reading-progress.js
@@ -1,51 +1,32 @@
 /* ==========================================================================
-	 Reading Progress Affix
-	 ========================================================================== */
-if (affix) {
-	affix.dispose();
-	affix = null;
-}
-
-var topicsElement = document.querySelector('.guide-topics');
-if(topicsElement) {
-	var docsNavOffsetTop = topicsElement ? 450 : 330;
-	var docsNavOffsetBottom = 60;
-
-	var affix = new metal.Affix({
-		element: '.docs-nav',
-		offsetTop: docsNavOffsetTop,
-		offsetBottom: docsNavOffsetBottom
-	});
-}
-
-/* ==========================================================================
 	 Reading Progress Resize Width
 	 ========================================================================== */
 
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener('DOMContentLoaded', function(event) {
 	resizeReadingProgress();
-
-	// TODO : check why senna.dataAttributeHandler.getApp() is starting null
-	var timeoutId = setTimeout(() => {
-		var app = senna.dataAttributeHandler.getApp();
-		if(app) {
-			clearTimeout(timeoutId);
-			app.on('endNavigate', endNavigate);
-		}
-	}, 500);
 });
 
-function endNavigate(e) {
+window.addEventListener('load', function(event) {
+	var app = senna.dataAttributeHandler.getApp();
+	app.on('endNavigate', endNavigateReadingProgress);
+});
+
+function endNavigateReadingProgress(e) {
 	resizeReadingProgress();
 }
 
 function resizeReadingProgress() {
 	var readingProgressContent = document.querySelector('.docs-nav-container');
-	var docsNav = readingProgressContent.childNodes[0];
-	docsNav.style.width = readingProgressContent.offsetWidth + "px";
 
-	window.addEventListener('resize', function() {
-		docsNav.style.width = readingProgressContent.offsetWidth + "px";
-	});
+	if(readingProgressContent) {
+		var docsNav = readingProgressContent.childNodes[0];
+		docsNav.style.width = readingProgressContent.offsetWidth + 'px';
+
+		//TODO : check if it's really necessary
+		window.addEventListener('resize', function() {
+			docsNav.style.width = readingProgressContent.offsetWidth + 'px';
+		});
+	}
+
 }
 
