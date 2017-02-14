8b2517b94f0ab39cba4b10cf100815c8f0ce70b9
Zeno Rocha <zno.rocha@gmail.com>
2017-02-13 17:28:03 -0800

    Update files to Electric v1.3.0 changes

--- ui/src/components/Sidebar.js
+++ ui/src/components/Sidebar.js
@@ -7,9 +7,15 @@ import Toggler from 'metal-toggler';
 import templates from './Sidebar.soy';
 
 class Sidebar extends Component {
+	attached() {
+		this._toggler = new Toggler({
 			content: '.sidebar-toggler-content',
 			header: '.sidebar-header'
 		});
+	}
+
+	disposed() {
+		this._toggler.dispose();
 	}
 };
 
