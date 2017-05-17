c0a33752952103cdbd67a14ecfc55e42ed66f0a2
Zeno Rocha <zno.rocha@gmail.com>
2017-05-17 15:28:41 -0700

    Build

---
+++ ui/dist/js/bundles/utils.js
@@ -0,0 +1,26 @@
+var pageComponent =
+webpackJsonppageComponent([137],{
+
+/***/ 32:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+exports.dispatchGlobalState = dispatchGlobalState;
+function dispatchGlobalState() {
+	if (window.electricPageComponent) {
+		window.electricPageComponent.setState({
+			element: '#pageComponent',
+			page: page,
+			site: data.site
+		});
+	}
+}
+
+/***/ })
+
+},[32]);
