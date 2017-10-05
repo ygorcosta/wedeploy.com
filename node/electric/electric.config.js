47bf572aac487d72bb0ed74c2de9f688c24c5dbc
Robert Frampton <robert.frampton@liferay.com>
2017-10-05 11:05:40 -0700

    Search wasn't working because electric project doesn't have an index page (#281)

--- node/electric/electric.config.js
+++ node/electric/electric.config.js
@@ -3,6 +3,13 @@
 var marble = require('marble');
 
 module.exports = {
+	frontMatterHook: function(siteData) {
+		if (!siteData.index.url) {
+			siteData.index.url = 'https://wedeploy.com/'
+		}
+
+		return siteData;
+	},
 	metalComponents: ['electric-marble-components'],
 	sassOptions: {
 		includePaths: ['node_modules', marble.src],
