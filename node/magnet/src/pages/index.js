232ac083e55f184478ce1b1471e58cd01645c599
Zeno Rocha <zno.rocha@gmail.com>
2018-03-08 15:42:04 -0800

    Adds missing <head> tag

--- node/magnet/src/pages/index.js
+++ node/magnet/src/pages/index.js
@@ -42,27 +42,29 @@ export default class Index extends Component {
   static renderLayout(req, content) {
     return `<!doctype html>
 <html lang="en" dir="ltr">
-  <title>WeDeploy™</title>
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
-  <meta name="google-site-verification" content="-IGQZ8MP0xKLi1s43kLfzb-QRKs_jlbk0hrTusIZaK0">
-  <meta name="twitter:card" content="summary_large_image">
-  <meta property="og:description" content="Forget about infrastructure. Dedicate your time to what really matters: building great apps.">
-  <meta property="og:image" content="https://wedeploy.com/images/open_graph.png">
-  <meta property="og:image:alt" content="WeDeploy Logo">
-  <meta property="og:image:height" content="630">
-  <meta property="og:image:width" content="1200">
-  <meta property="og:site_name" content="WeDeploy">
-  <meta property="og:title" content="WeDeploy™">
-  <meta property="og:type" content="website">
-  <meta property="og:url" content="https://wedeploy.com/">
-  <link rel="icon" href="/images/favicon.ico" type="image/x-icon">
-  <link rel="stylesheet" href="/static/styles/main.css">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Mono:500,700">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:900&amp;text=%E2%80%A2">
-  <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.5.0/styles/arta.min.css">
-  <script src="https://rawgit.com/web-animations/web-animations-js/master/web-animations.min.js"></script>
+  <head>
+    <title>WeDeploy™</title>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+    <meta name="google-site-verification" content="-IGQZ8MP0xKLi1s43kLfzb-QRKs_jlbk0hrTusIZaK0">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta property="og:description" content="Forget about infrastructure. Dedicate your time to what really matters: building great apps.">
+    <meta property="og:image" content="https://wedeploy.com/images/open_graph.png">
+    <meta property="og:image:alt" content="WeDeploy Logo">
+    <meta property="og:image:height" content="630">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:site_name" content="WeDeploy">
+    <meta property="og:title" content="WeDeploy™">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://wedeploy.com/">
+    <link rel="icon" href="/images/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="/static/styles/main.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Mono:500,700">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:900&amp;text=%E2%80%A2">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.5.0/styles/arta.min.css">
+    <script src="https://rawgit.com/web-animations/web-animations-js/master/web-animations.min.js"></script>
+  </head>
   <body>
     ${highlightCodeSnippetsFromMarkup(content)}
   </body>
